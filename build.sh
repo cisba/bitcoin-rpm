6372e968d58489d77b4c548e52b14d89c817ac22
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2019-03-04 19:46:35 +0100

    spec derived from ringingliberty, plus build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# check or setup rpmbuild toos and stuff
+sudo yum install -y rpmdevtools rpmlint selinux-policy-devel
+[ ! -f ~/.rpmmacros ] && rpmdev-setuptree
+TOPDIR=$(rpm --eval "%{_topdir}")
+[ ! -d ${TOPDIR} ] && rpmdev-setuptree
+read
+
+# prepare files in the right place
+cp -p src/* ${TOPDIR}/SOURCES
+cd ${TOPDIR}/SPECS
+mv ${TOPDIR}/SOURCES/bitcoin.spec . 
+
+# do the job
+rpmbuild --undefine=_disable_source_fetch -ba bitcoin.spec
+
+
+
